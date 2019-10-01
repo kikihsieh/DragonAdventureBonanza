add11dde162d9033d5025615279344008e71962b
Karen <karenhong4@gmail.com>
2019-09-30 19:28:52 -0700

    Only snap when movement in one direction passes a threshold

--- template/src/camera.cpp
+++ template/src/camera.cpp
@@ -6,24 +6,39 @@ void Camera::init(vec2 dim) {
     m_center.x = dim.x / 2.0f;
     m_center.y = dim.y / 2.0f;
 
-    m_snap_speed = 12.f;
     m_offset_x = m_dim.x / 8.f;
+
+    m_snap_speed = 12.f;
+    m_snap_dist = m_dim.x / 4.f;
+
+    m_snap_threshold_f = true;
+    m_snap_threshold_b = false;
 }
 
 void Camera::update(vec2 player_pos, bool moving_forwards) {
     if (moving_forwards) {
-        float forwards_center_x = player_pos.x + m_offset_x;
-        if (m_center.x < forwards_center_x) {
-            m_center.x = fmin(m_center.x + m_snap_speed, forwards_center_x);
+        m_snap_threshold_b = false;
+        if (m_snap_threshold_f) {
+            float forwards_center_x = player_pos.x + m_offset_x;
+            if (m_center.x < forwards_center_x) {
+                m_center.x = fmin(m_center.x + m_snap_speed, forwards_center_x);
+            }
+        } else if (player_pos.x > (m_center.x + m_snap_dist)) {
+            m_snap_threshold_f = true;
         }
     } else {
-        float backwards_center_x = player_pos.x - m_offset_x;
-        if (m_center.x > backwards_center_x) {
-            m_center.x = fmax(m_center.x - m_snap_speed, backwards_center_x);
+        m_snap_threshold_f = false;
+        if (m_snap_threshold_b) {
+            float backwards_center_x = player_pos.x - m_offset_x;
+            if (m_center.x > backwards_center_x) {
+                m_center.x = fmax(m_center.x - m_snap_speed, backwards_center_x);
+            }
+        } else if (player_pos.x < (m_center.x - m_snap_dist)) {
+                m_snap_threshold_b = true;
         }
     }
 }
 
 float Camera::compute_translation_x() {
-    return (-2.0f * m_center.x / m_dim.x);
+    return -2.0f * m_center.x / m_dim.x;
 }
--- template/src/camera.hpp
+++ template/src/camera.hpp
@@ -1,7 +1,6 @@
 #ifndef DAB_CAMERA_HPP
 #define DAB_CAMERA_HPP
 
-
 #include "common.hpp"
 #include "player.hpp"
 
@@ -17,10 +16,13 @@ private:
     vec2 m_center;
     vec2 m_dim;
 
-    float m_offset_x; // by default amount the camera center is offset from the player
+    float m_offset_x; // the default amount the camera center is offset from the player
 
     float m_snap_speed;
-};
+    float m_snap_dist; // when player exceeds this distance from the camera center, camera will snap to default position
 
+    bool m_snap_threshold_b; // true if camera should snap to default backwards position
+    bool m_snap_threshold_f; // true if camera should snap to default forwards position
+};
 
 #endif //DAB_CAMERA_HPP
