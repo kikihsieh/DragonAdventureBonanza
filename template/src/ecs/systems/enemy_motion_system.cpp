d5619e2b5880b2fb3b41c94c055d174df120c6a8
Mayesha Kabir <mayeshakabir@gmail.com>
2019-10-21 15:23:10 -0700

    fix

--- template/src/ecs/systems/enemy_motion_system.cpp
+++ template/src/ecs/systems/enemy_motion_system.cpp
@@ -16,18 +16,6 @@ bool EnemyMotionSystem::init(std::list<Entity> *entities, const std::map<int, Ti
     m_tiles = tiles;
     m_entities = entities;
     
-    /*for (auto &entity : *m_entities){
-        if (!entity.enemyai){
-            continue;
-        }
-        if (init_direction == 1){
-            entity.is_facing_forward = false; // walk towards left
-        } else {
-            entity.is_facing_forward = false;
-            
-        }
-    }*/
-    
     return true;
 }
 
@@ -36,34 +24,23 @@ void EnemyMotionSystem::update(float ms) {
         if (!entity.physics || !entity.collider || !entity.enemyai){
             continue;
         }
-        
-        if (entity.is_facing_forward){
-            entity.physics->velocity.x = entity.physics->walk_speed;
-        } else {
-            entity.physics->velocity.x = -1* entity.physics->walk_speed;
-        }
 
-        
         float e_height = entity.drawable->texture->height * entity.scale.x;
         float e_width = entity.drawable->texture->width * entity.scale.y;
         
         std::pair<int, int> enemy_tile_pos = TileMap::get_tile_pos_from_coord(entity.position.x, entity.position.y, {e_width, e_height});
-        
         std::pair<int, int> platform_tile_pos = {enemy_tile_pos.first, enemy_tile_pos.second + 1};
         
         if (entity.is_facing_forward && m_tiles.count(TileMap::hash(platform_tile_pos.first+1, platform_tile_pos.second)) == 0) {
             entity.is_facing_forward = !(entity.is_facing_forward);
-            // change direction
-               entity.physics->velocity.x *= -1;
+            entity.physics->velocity.x *= -1;
         }
         else if (!entity.is_facing_forward && m_tiles.count(TileMap::hash(platform_tile_pos.first, platform_tile_pos.second)) == 0) {
             entity.is_facing_forward = !(entity.is_facing_forward);
-            // change direction
             entity.physics->velocity.x *= -1;
         }
         if (entity.collider->left || entity.collider->right){
             entity.is_facing_forward = !(entity.is_facing_forward);
-            // change direction 
             entity.physics->velocity.x *= -1;
         }
     }
