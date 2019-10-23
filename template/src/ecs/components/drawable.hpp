49355e3232551416fb3ce921705a3d82354b4647
chid123 <chid123@students.cs.ubc.ca>
2019-10-23 14:55:59 -0700

    confirm git setting is correct - Desta

--- template/src/ecs/components/drawable.hpp
+++ template/src/ecs/components/drawable.hpp
@@ -25,7 +25,6 @@ struct Drawable {
 
     TexturedVertex vertices[4];
     std::shared_ptr<Texture> texture;
-
     mat3 transform;
 
     // Effect component of Entity for Vertex and Fragment shader, which are then put(linked) together in a
