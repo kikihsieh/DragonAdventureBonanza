b722fdb7526c646af1173e645c09c3f22c6d1128
Arden Allen <apallen@students.cs.ubc.ca>
2019-11-21 15:32:25 -0800

    Dragon turns red

---
+++ template/shaders/player.fs.glsl
@@ -0,0 +1,22 @@
+#version 330
+
+// From vertex shader
+in vec2 texcoord;
+
+// Application data
+uniform sampler2D sampler0;
+uniform vec3 fcolor;
+uniform float invicibility;
+
+// Output color
+layout(location = 0) out  vec4 color;
+
+void main()
+{
+    if (invicibility == 0.0) {
+        color = vec4(fcolor, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y));
+    }
+    else {
+        color = vec4(0.7, 0.0, 0.0, 0.0) + (vec4(fcolor, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y)));
+    }
+}
