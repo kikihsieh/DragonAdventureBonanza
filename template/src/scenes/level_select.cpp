fb7d7b29c93ce187271f1a26341e1b39a7454fa5
Arden Allen <apallen@students.cs.ubc.ca>
2019-11-21 22:42:33 -0800

    level select done

--- template/src/scenes/level_select.cpp
+++ template/src/scenes/level_select.cpp
@@ -12,16 +12,28 @@ bool LevelSelect::init() {
     m_buttons.emplace_back(forest);
     m_entities.emplace_back(forest);
     Button cave(textures_path("level-select/button-cave.png"));
+    if (m_unlocked_levels->at(CAVE) == false) {
+        cave.drawable->texture_path = textures_path("level-select/button-lock.png");
+        cave.active = false;
+    }
     cave.position = {462.5,400.f};
     cave.m_button_callback = [this](){load_scene(CAVE);};
     m_buttons.emplace_back(cave);
     m_entities.emplace_back(cave);
     Button snow(textures_path("level-select/button-snow-mountain.png"));
+    if (m_unlocked_levels->at(SNOW_MOUNTAIN) == false) {
+        snow.drawable->texture_path = textures_path("level-select/button-lock.png");
+        snow.active = false;
+    }
     snow.position = {737.5,400.f};
     snow.m_button_callback = [this](){load_scene(SNOW_MOUNTAIN);};
     m_buttons.emplace_back(snow);
     m_entities.emplace_back(snow);
-    Button boss(textures_path("level-select/button-lock.png"));
+    Button boss(textures_path("level-select/button-night-sky.png"));
+    if (true) {
+        boss.drawable->texture_path = textures_path("level-select/button-lock.png");
+        boss.active = false;
+    }
     boss.position = {1012.5,400.f};
     boss.active = false;
 //    boss.m_button_callback = [this](){load_scene();};
