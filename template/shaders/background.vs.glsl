0303143a972eb81c9e68e8c27223d155e8ad0377
Arden Allen <apallen@students.cs.ubc.ca>
2019-11-25 20:57:41 -0800

    Basic parallax working

--- template/shaders/background.vs.glsl
+++ template/shaders/background.vs.glsl
@@ -3,11 +3,15 @@
 layout(location = 0) in vec4 in_position;
 
 out vec2 uv;
+uniform mat3 projection;
+uniform vec2 dims;
 
 void main()
 {
+    vec3 offset = projection * vec3(600.f, 400.f, 1.f);
     gl_Position = in_position;
 
     // Convert to the [0, 1] range of UV coordinate
-    uv = (in_position.xy + vec2(1.05, 1.05)) / 2.1;
+//    uv = ((in_position.xy / vec2(14400.0/1200.0, 1632.0/800) + vec2(1.0, 1.0)) / 2) - (vec2(offset.x, offset.y)/vec2(14400.0/1200.0, 1632.0/800));
+    uv = (((in_position.xy + vec2(1.0, 1.0)) / 2) - 0.25*vec2(offset.x, 0.0));
 }
