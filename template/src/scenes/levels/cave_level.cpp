5032be7dbf031b9a184ee46f52ff4bd273bdce07
Arden Allen <apallen@students.cs.ubc.ca>
2019-11-26 16:35:28 -0800

    clean up again

--- template/src/scenes/levels/cave_level.cpp
+++ template/src/scenes/levels/cave_level.cpp
@@ -18,19 +18,6 @@ bool CaveLevel::init_player() {
     Player player;
     m_entities.emplace_back(player);
     m_player = &m_entities.back();
-    
-    if (SDL_Init(SDL_INIT_AUDIO) < 0)
-    {
-        fprintf(stderr, "Failed to initialize SDL Audio");
-        return false;
-    }
-    
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) == -1)
-    {
-        fprintf(stderr, "Failed to open audio device");
-        return false;
-    }
-
     return true;
 }
 
