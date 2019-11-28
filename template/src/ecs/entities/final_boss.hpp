3a27859aee8383107565b6bb788aece31f14264b
Arden Allen <apallen@students.cs.ubc.ca>
2019-11-28 15:52:36 -0800

    Adjustments for boss spritesheet

--- template/src/ecs/entities/final_boss.hpp
+++ template/src/ecs/entities/final_boss.hpp
@@ -6,16 +6,15 @@
 struct FinalBoss : public Entity {
     FinalBoss(std::shared_ptr<Texture> texture, vec2 pos) {
         scale = { 0.35f, 0.35f };
-        texture_size = {(float) texture->width, (float) texture->height};
+//        texture_size = {(float) texture->width, (float) texture->height};
 
         drawable = new Drawable();
-        drawable->fs_shader = shader_path("textured.fs.glsl");
-        drawable->vs_shader = shader_path("textured.vs.glsl");
+        drawable->fs_shader = shader_path("animated.fs.glsl");
+        drawable->vs_shader = shader_path("animated.vs.glsl");
         drawable->texture = texture;
 
         position = pos;
 
-        position = pos;
         physics = new Physics();
         physics->gravity = 0;
         physics->walk_speed = 30.f;
@@ -33,6 +32,7 @@ struct FinalBoss : public Entity {
         animatable = new Animatable();
         animatable->num_rows = 1;
         animatable->num_columns = 10;
+        animatable->frame_index = {0, 0};
     }
 };
 
