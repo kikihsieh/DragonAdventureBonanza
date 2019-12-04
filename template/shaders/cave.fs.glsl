6adddd378c7f886d189e265a0d27ca27dcc7b5d7
Arden Allen <apallen@students.cs.ubc.ca>
2019-12-04 15:33:55 -0800

    darker?

--- template/shaders/cave.fs.glsl
+++ template/shaders/cave.fs.glsl
@@ -13,18 +13,35 @@ layout(location = 0) out vec4 color;
 
 void main()
 {
-    color = vec4(0.0, 0.0, 0.0, 0.7);
-    float radius = distance((projection * vec3(player, 1.0)).xy * vec2(12.0/8.0, 1.0), vec2(vpos.x*(12.0/8.0), vpos.y));
-    if(radius < 0.7) {
-        color = vec4(0.0, 0.0, 0.0, radius);
-    }
+    color = vec4(0.0, 0.0, 0.0, 0.9);
+    float lightDist = 1000;
     for (int i = 0; i < 10; i++) {
-        radius = distance((projection * vec3(lights[i], 1.0)).xy * vec2(12.0/8.0, 1.0), vec2(vpos.x*(12.0/8.0), vpos.y));
+        float radius = distance((projection * vec3(lights[i], 1.0)).xy * vec2(12.0/8.0, 1.0), vec2(vpos.x*(12.0/8.0), vpos.y));
+        float dist = distance((projection * vec3(lights[i], 1.0)).xy * vec2(12.0/8.0, 1.0), (projection * vec3(player, 1.0)).xy * vec2(12.0/8.0, 1.0));
+        if (dist < lightDist){
+            lightDist = dist;
+        }
         if (radius < 1.0) {
             color.xyz += (1.0 - radius) * 0.6 * vec3(1.0, 0.7, 0.0);
+            color.a = 0.5 + radius*(0.4);
+//        } else {
+//            float dist = distance((projection * vec3(lights[i], 1.0)).xy, (projection * vec3(player, 1.0)).xy);
+//            if (dist > 1.5) {
+//                float radius = distance((projection * vec3(player, 1.0)).xy * vec2(12.0/8.0, 1.0), vec2(vpos.x*(12.0/8.0), vpos.y));
+//                if(radius < 0.5) {
+//                    color = vec4(0.0, 0.0, 0.0, radius + 0.2);
+//                }
+//            }
+
         }
         if (i == numLights)
             break;
 
+    }
+    if (lightDist > 1.5) {
+        float radius = distance((projection * vec3(player, 1.0)).xy * vec2(12.0/8.0, 1.0), vec2(vpos.x*(12.0/8.0), vpos.y));
+        if(radius < 0.5) {
+            color = vec4(0.0, 0.0, 0.0,0.2+radius*(1.4));
+        }
     }
 }
