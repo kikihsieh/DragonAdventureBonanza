292152bc6cc173a996359baa64f3161ad4cadd69
Mayesha Kabir <mayeshakabir@gmail.com>
2019-12-05 23:12:30 -0800

    added level buttons

--- template/src/scenes/start_menu.cpp
+++ template/src/scenes/start_menu.cpp
@@ -7,11 +7,13 @@ StartMenu::StartMenu() {
 bool StartMenu::init() {
     Button start(textures_path("start.png"));
     start.position = {600.f,400.f};
+    start.scale = {0.75f, 0.75f};
     start.m_button_callback = [this](){load_scene(STORYLINE);};
     m_buttons.emplace_back(start);
 
-    Button level_select(textures_path("start.png"));
-    level_select.position = {600.f,600.f};
+    Button level_select(textures_path("levels.png"));
+    level_select.position = {600.f,550.f};
+    level_select.scale = {0.75f, 0.75f};
     level_select.m_button_callback = [this](){load_scene(LEVEL_SELECT);};
     m_buttons.emplace_back(level_select);
 
@@ -21,9 +23,9 @@ bool StartMenu::init() {
     help.m_button_callback = [this]() {drawHelp = !drawHelp;};
     m_buttons.emplace_back(help);
 
-    Button exit(textures_path("exit.png"));
+    Button exit(textures_path("buttons/exit.png"));
     exit.position = {50.f,750.f};
-    exit.scale = {0.35f, 0.35f};
+    exit.scale = {0.5f, 0.5f};
     exit.m_button_callback = [this](){exit_game();};
     m_buttons.emplace_back(exit);
     
