33ee7de8afe8259e7edfe1721a1c1d2aa4a787b7
kikih <kikih@student.cs.ubc.ca>
2019-12-07 21:26:02 -0800

    animated static torches & fixed level

--- template/src/scenes/victory.cpp
+++ template/src/scenes/victory.cpp
@@ -5,6 +5,7 @@ Victory::Victory() {
 
 bool Victory::init() {
     m_background_music = Mix_LoadMUS(audio_path("victory.wav"));
+    Mix_VolumeMusic(MIX_MAX_VOLUME/2);
     Scene::init();
     return true;
 }
